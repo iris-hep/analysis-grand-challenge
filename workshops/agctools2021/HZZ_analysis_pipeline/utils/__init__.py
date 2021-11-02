b061258797d8711838d06f570a49e8ce08c162d4
Alexander Held <alexander.held@cern.ch>
2021-11-02 15:44:58 +0100

    move errorband plotting to utility

--- workshops/agctools2021/HZZ_analysis_pipeline/utils/__init__.py
+++ workshops/agctools2021/HZZ_analysis_pipeline/utils/__init__.py
@@ -2,9 +2,13 @@ import logging
 import os
 import shutil
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 def clean_up():
-    # clean up files that may be left over from previous running (not needed, just to simplify debugging)
+    # clean up files that may be left over from previous running (not needed, just to
+    # simplify debugging)
     for path in ["histograms/", "figures/"]:
         if os.path.exists(path):
             shutil.rmtree(path)
@@ -13,3 +17,32 @@ def clean_up():
 def set_logging():
     logging.basicConfig(format="%(levelname)s - %(name)s - %(message)s")
     logging.getLogger("cabinetry").setLevel(logging.INFO)
+
+
+def plot_errorband(bin_edge_low, bin_edge_high, num_bins, histograms):
+    # bin parameters
+    step_size = (bin_edge_high - bin_edge_low) / num_bins
+    bin_centers = histograms["data"].axes[0].centers
+
+    # calculate background statistical uncertainty
+    mc_histogram_sum = histograms["MC"][:, :, "nominal"].project("mllll").values()
+    mc_err = np.sqrt(histograms["MC"][:, :, "nominal"].project("mllll").variances())
+
+    # plot background statistical uncertainty
+    plt.bar(
+        bin_centers,  # x
+        2 * mc_err,  # heights
+        alpha=0.5,  # half transparency
+        bottom=mc_histogram_sum - mc_err,
+        color="none",
+        hatch="////",
+        width=step_size,
+        label="Stat. Unc.",
+    )
+
+    # tune plot appearance
+    main_axes = plt.gca()
+    main_axes.set_xlim(left=bin_edge_low, right=bin_edge_high)
+    main_axes.set_ylim(bottom=0, top=np.amax(histograms["data"].values()) * 1.6)
+    main_axes.set_ylabel("Events / " + str(step_size) + " GeV")
+    main_axes.legend(frameon=False)
