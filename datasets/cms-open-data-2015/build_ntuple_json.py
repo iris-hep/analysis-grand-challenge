8e35e7d597d3a5d83eb7217c2b28132bd4ced9ca
Alexander Held <45009355+alexander-held@users.noreply.github.com>
2022-04-19 14:41:21 +0200

    feat: update single top structure and add event counting (#39)
    * update single top structure
    * add event counting
    * fix path in a ttbar file

--- datasets/cms-open-data-2015/build_ntuple_json.py
+++ datasets/cms-open-data-2015/build_ntuple_json.py
@@ -1,5 +1,10 @@
 from collections import defaultdict
 import json
+import time
+
+import uproot
+
+RUN_AT_UNL = False
 
 
 def get_paths(process, recids):
@@ -14,15 +19,49 @@ def get_paths(process, recids):
             prefix_eos = "root://eospublic.cern.ch//eos/opendata/cms/"
         else:
             prefix_eos = "root://eospublic.cern.ch//eos/opendata/cms/mc/"
-        prefix_unl = "root://xrootd-local.unl.edu:1094//store/user/AGC/datasets/"
+        prefix_unl = "http://xrootd-local.unl.edu:1094//store/user/AGC/datasets/"
         all_files += [f.strip().replace(prefix_eos, prefix_unl) for f in files]
 
     return all_files
 
 
-def update_dict(file_dict, process, sample, recid):
+def num_events_list(files):
+    num_events = []
+    t0 = time.time()
+    warned = False
+    for i, filename in enumerate(files):
+        if i % 10 == 0 or i == len(files) - 1:
+            print(f"{i+1} / {len(files)} in {time.time() - t0:.0f} s")
+        if RUN_AT_UNL:
+            # read locally at UNL Tier-3
+            filename = filename.replace(
+                "http://xrootd-local.unl.edu:1094/", "/mnt/t2ceph/cms"
+            )
+        elif not warned:
+            print(
+                "WARNING: event counting over http, this may be slow (set RUN_AT_UNL)"
+            )
+            warned = True
+        with uproot.open(filename) as f:
+            num_events.append(f["events"].num_entries)
+    return num_events
+
+
+def update_dict(file_dict, process, variation, recid):
+    print(f"process: {process}")
     files = get_paths(process, recid)
-    file_dict[process].update({sample: files})
+    nevts_list = num_events_list(files)
+    file_dict[process].append(
+        {
+            "variation": variation,
+            "files": [{"path": f, "nevts": n} for f, n in zip(files, nevts_list)],
+            "nevts_total": sum(nevts_list),
+        }
+    )
+
+    # create partial backup
+    write_to_file(file_dict, f"ntuples_{process}_{variation}.json")
+
     return file_dict
 
 
@@ -33,22 +72,22 @@ def write_to_file(file_dict, path):
 
 
 if __name__ == "__main__":
-    file_dict = defaultdict(dict)
+    file_dict = defaultdict(list)
 
     # ttbar
-    update_dict(file_dict, "ttbar", "nominal", 19980)  # can also add 19981
+    update_dict(file_dict, "ttbar", "nominal", [19980, 19981])
     update_dict(file_dict, "ttbar", "scaledown", 19983)
     update_dict(file_dict, "ttbar", "scaleup", 19985)
     update_dict(file_dict, "ttbar", "ME_var", 19978)
     update_dict(file_dict, "ttbar", "PS_var", 19999)
 
     # single top
-    update_dict(file_dict, "single_top", "s_chan", 19394)
-    update_dict(file_dict, "single_top", "t_chan", [19406, 19408])
-    update_dict(file_dict, "single_top", "tW", [19412, 19419])
+    update_dict(file_dict, "single_top_s_chan", "nominal", 19394)
+    update_dict(file_dict, "single_top_t_chan", "nominal", [19406, 19408])
+    update_dict(file_dict, "single_top_tW", "nominal", [19412, 19419])
 
     # W+jets
-    update_dict(file_dict, "wjets", "nominal", 20547)  # can also add 20548
+    update_dict(file_dict, "wjets", "nominal", [20547, 20548])
 
     # data
     update_dict(file_dict, "data", "nominal", [24119, 24120])
