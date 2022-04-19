8bfd095139af7e474124c8514715d9e23ef45ccb
Alexander Held <alexander.held@cern.ch>
2022-04-19 14:23:48 +0200

    improve json structure

--- datasets/cms-open-data-2015/build_ntuple_json.py
+++ datasets/cms-open-data-2015/build_ntuple_json.py
@@ -25,31 +25,43 @@ def get_paths(process, recids):
     return all_files
 
 
-def num_events(files):
-    num_events_total = 0
+def num_events_list(files):
+    num_events = []
     t0 = time.time()
+    warned = False
     for i, filename in enumerate(files):
-        if i % 10 == 0 or i == len(files) - 1:
+        if i % 1000 == 0 or i == len(files) - 1:
             print(f"{i+1} / {len(files)} in {time.time() - t0:.0f} s")
         if RUN_AT_UNL:
             # read locally at UNL Tier-3
             filename = filename.replace(
                 "http://xrootd-local.unl.edu:1094/", "/mnt/t2ceph/cms"
             )
-        else:
+        elif not warned:
             print(
                 "WARNING: event counting over http, this may be slow (set RUN_AT_UNL)"
             )
+            warned = True
         with uproot.open(filename) as f:
-            num_events_total += f["events"].num_entries
-    return num_events_total
+            num_events.append(f["events"].num_entries)
+    return num_events
 
 
 def update_dict(file_dict, process, variation, recid):
     print(f"process: {process}")
     files = get_paths(process, recid)
-    nevts = num_events(files)
-    file_dict[process].update({"variation": variation, "files": files, "nevts": nevts})
+    nevts_list = num_events_list(files)
+    file_dict[process].append(
+        {
+            "variation": variation,
+            "files": [{"path": f, "nevts": n} for f, n in zip(files, nevts_list)],
+            "nevts_total": sum(nevts_list),
+        }
+    )
+
+    # create partial backup
+    write_to_file(file_dict, f"ntuples_{process}_{variation}.json")
+
     return file_dict
 
 
@@ -60,7 +72,7 @@ def write_to_file(file_dict, path):
 
 
 if __name__ == "__main__":
-    file_dict = defaultdict(dict)
+    file_dict = defaultdict(list)
 
     # ttbar
     update_dict(file_dict, "ttbar", "nominal", [19980, 19981])
