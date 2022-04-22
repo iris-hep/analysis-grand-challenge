d18f9820d49922fac2dc75397d969887bdba7aad
Alexander Held <alexander.held@cern.ch>
2022-04-22 22:11:09 +0200

    move more to utils

--- analyses/cms-open-data-ttbar/utils/__init__.py
+++ analyses/cms-open-data-ttbar/utils/__init__.py
@@ -1,3 +1,8 @@
+import json
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
 def get_client(af="coffea_casa"):
     if af == "coffea_casa":
         from dask.distributed import Client
@@ -24,3 +29,50 @@ def get_client(af="coffea_casa"):
         raise NotImplementedError(f"unknown analysis facility: {af}")
 
     return client
+
+
+def set_style():
+    mpl.style.use("ggplot")
+    plt.rcParams["axes.facecolor"] = "none"
+    plt.rcParams["axes.edgecolor"] = "222222"
+    plt.rcParams["axes.labelcolor"] = "222222"
+    plt.rcParams["xtick.color"] = "222222"
+    plt.rcParams["ytick.color"] = "222222"
+    plt.rcParams["font.size"] = 12
+    plt.rcParams['text.color'] = "222222"
+
+
+def construct_fileset(n_files_max_per_sample):
+    # using https://atlas-groupdata.web.cern.ch/atlas-groupdata/dev/AnalysisTop/TopDataPreparation/XSection-MC15-13TeV.data
+    # for reference
+    # x-secs are in pb
+    xsec_info = {
+        "ttbar": 396.87 + 332.97, # nonallhad + allhad, keep same x-sec for all
+        "single_top_s_chan": 2.0268 + 1.2676,
+        "single_top_t_chan": (36.993 + 22.175)/0.252,  # scale from lepton filter to inclusive
+        "single_top_tW": 37.936 + 37.906,
+        "wjets": 61457 * 0.252,  # e/mu+nu final states
+        "data": None
+    }
+
+    # list of files
+    with open("ntuples.json") as f:
+        file_info = json.load(f)
+
+    # process into "fileset" summarizing all info
+    fileset = {}
+    for process in file_info.keys():
+        if process == "data":
+            continue  # skip data
+
+        for variation in file_info[process].keys():
+            file_list = file_info[process][variation]["files"]
+            if n_files_max_per_sample != -1:
+                file_list = file_list[:n_files_max_per_sample]  # use partial set of samples
+
+            file_paths = [f["path"] for f in file_list]
+            nevts_total = sum([f["nevts"] for f in file_list])
+            metadata = {"process": process, "variation": variation, "nevts": nevts_total, "xsec": xsec_info[process]}
+            fileset.update({f"{process}__{variation}": {"files": file_paths, "metadata": metadata}})
+
+    return fileset
