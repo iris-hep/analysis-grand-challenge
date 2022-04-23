caf8ad842300aa4e3cfba7145c66970c928480ec
Alexander Held <alexander.held@cern.ch>
2022-04-23 13:41:20 +0200

    fix jet addition for ServiceX and add XCache paths

--- analyses/cms-open-data-ttbar/utils/__init__.py
+++ analyses/cms-open-data-ttbar/utils/__init__.py
@@ -42,7 +42,7 @@ def set_style():
     plt.rcParams['text.color'] = "222222"
 
 
-def construct_fileset(n_files_max_per_sample):
+def construct_fileset(n_files_max_per_sample, use_xcache=False):
     # using https://atlas-groupdata.web.cern.ch/atlas-groupdata/dev/AnalysisTop/TopDataPreparation/XSection-MC15-13TeV.data
     # for reference
     # x-secs are in pb
@@ -71,6 +71,8 @@ def construct_fileset(n_files_max_per_sample):
                 file_list = file_list[:n_files_max_per_sample]  # use partial set of samples
 
             file_paths = [f["path"] for f in file_list]
+            if use_xcache:
+                file_paths = [f.replace("https://xrootd-local.unl.edu:1094", "root://red-xcache1.unl.edu") for f in file_paths]
             nevts_total = sum([f["nevts"] for f in file_list])
             metadata = {"process": process, "variation": variation, "nevts": nevts_total, "xsec": xsec_info[process]}
             fileset.update({f"{process}__{variation}": {"files": file_paths, "metadata": metadata}})
