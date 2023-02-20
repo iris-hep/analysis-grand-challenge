d2ab83532ae72e5ecd07f3f0702a442f26f95842
ekauffma <elmaka8700@gmail.com>
2023-02-20 13:53:59 +0000

    added use_servicex to metrics tracking

--- analyses/cms-open-data-ttbar/ttbar_analysis_pipeline.py
+++ analyses/cms-open-data-ttbar/ttbar_analysis_pipeline.py
@@ -492,26 +492,35 @@ all_histograms = all_histograms["hist"]
 print(f"\nexecution took {exec_time:.2f} seconds")
 
 # %%
-# track metrics for pure coffea setups
-if not USE_SERVICEX:
-    # update metrics
-    dataset_source = "/data" if fileset["ttbar__nominal"]["files"][0].startswith("/data") else "https://xrootd-local.unl.edu:1094" # TODO: xcache support
-    metrics.update({"walltime": exec_time, "num_workers": NUM_CORES, "af": AF_NAME, "dataset_source": dataset_source, "use_dask": USE_DASK,
-                    "systematics": SYSTEMATICS, "n_files_max_per_sample": N_FILES_MAX_PER_SAMPLE,
-                    "cores_per_worker": CORES_PER_WORKER, "chunksize": CHUNKSIZE, "disable_processing": DISABLE_PROCESSING, "io_file_percent": IO_FILE_PERCENT})
+# track metrics
+dataset_source = "/data" if fileset["ttbar__nominal"]["files"][0].startswith("/data") else "https://xrootd-local.unl.edu:1094" # TODO: xcache support
+metrics.update({
+    "walltime": exec_time, 
+    "num_workers": NUM_CORES, 
+    "af": AF_NAME, 
+    "dataset_source": dataset_source, 
+    "use_dask": USE_DASK, 
+    "use_servicex": USE_SERVICEX, 
+    "systematics": SYSTEMATICS, 
+    "n_files_max_per_sample": N_FILES_MAX_PER_SAMPLE,
+    "cores_per_worker": CORES_PER_WORKER, 
+    "chunksize": CHUNKSIZE, 
+    "disable_processing": DISABLE_PROCESSING, 
+    "io_file_percent": IO_FILE_PERCENT
+})
 
-    # save metrics to disk
-    if not os.path.exists("metrics"):
-        os.makedirs("metrics")
-    timestamp = time.strftime('%Y%m%d-%H%M%S')
-    metric_file_name = f"metrics/{AF_NAME}-{timestamp}.json"
-    with open(metric_file_name, "w") as f:
-        f.write(json.dumps(metrics))
+# save metrics to disk
+if not os.path.exists("metrics"):
+    os.makedirs("metrics")
+timestamp = time.strftime('%Y%m%d-%H%M%S')
+metric_file_name = f"metrics/{AF_NAME}-{timestamp}.json"
+with open(metric_file_name, "w") as f:
+    f.write(json.dumps(metrics))
 
-    print(f"metrics saved as {metric_file_name}")
-    #print(f"event rate per worker (full execution time divided by NUM_CORES={NUM_CORES}): {metrics['entries'] / NUM_CORES / exec_time / 1_000:.2f} kHz")
-    print(f"event rate per worker (pure processtime): {metrics['entries'] / metrics['processtime'] / 1_000:.2f} kHz")
-    print(f"amount of data read: {metrics['bytesread']/1000**2:.2f} MB")  # likely buggy: https://github.com/CoffeaTeam/coffea/issues/717
+print(f"metrics saved as {metric_file_name}")
+#print(f"event rate per worker (full execution time divided by NUM_CORES={NUM_CORES}): {metrics['entries'] / NUM_CORES / exec_time / 1_000:.2f} kHz")
+print(f"event rate per worker (pure processtime): {metrics['entries'] / metrics['processtime'] / 1_000:.2f} kHz")
+print(f"amount of data read: {metrics['bytesread']/1000**2:.2f} MB")  # likely buggy: https://github.com/CoffeaTeam/coffea/issues/717
 
 # %% [markdown]
 # ### Inspecting the produced histograms
