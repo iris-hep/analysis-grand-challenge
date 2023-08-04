24a069bb6b069aea7ad091e240f0c1489da92f0c
Alexander Held <45009355+alexander-held@users.noreply.github.com>
2023-08-04 13:17:39 +0200

    feat: support input files on public EOS (#185)
    * add new config option to support reading input files on EOS via xrootd

--- analyses/cms-open-data-ttbar/utils/__init__.py
+++ analyses/cms-open-data-ttbar/utils/__init__.py
@@ -49,7 +49,7 @@ def set_style():
     plt.rcParams['text.color'] = "222222"
 
 
-def construct_fileset(n_files_max_per_sample, use_xcache=False, af_name=""):
+def construct_fileset(n_files_max_per_sample, use_xcache=False, af_name="", input_from_eos=False):
     # using https://atlas-groupdata.web.cern.ch/atlas-groupdata/dev/AnalysisTop/TopDataPreparation/XSection-MC15-13TeV.data
     # for reference
     # x-secs are in pb
@@ -80,9 +80,11 @@ def construct_fileset(n_files_max_per_sample, use_xcache=False, af_name=""):
             file_paths = [f["path"] for f in file_list]
             if use_xcache:
                 file_paths = [f.replace("https://xrootd-local.unl.edu:1094", "root://red-xcache1.unl.edu") for f in file_paths]
-            if af_name == "ssl-dev":
+            elif af_name == "ssl-dev":
                 # point to local files on /data
                 file_paths = [f.replace("https://xrootd-local.unl.edu:1094//store/user/", "/data/alheld/") for f in file_paths]
+            elif input_from_eos:
+                file_paths = [f.replace("https://xrootd-local.unl.edu:1094//store/user/AGC/nanoAOD", "root://eospublic.cern.ch//eos/opendata/cms/upload/agc/1.0.0/") for f in file_paths]
             nevts_total = sum([f["nevts"] for f in file_list])
             metadata = {"process": process, "variation": variation, "nevts": nevts_total, "xsec": xsec_info[process]}
             fileset.update({f"{process}__{variation}": {"files": file_paths, "metadata": metadata}})
