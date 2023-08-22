4a6f5231b8b6606b9062a4ffabee170309858332
TatianaOvsiannikova <43534935+TatianaOvsiannikova@users.noreply.github.com>
2023-08-22 12:06:52 +0200

    docs: clarify ServiceX setup instructions (#188)
    * ServiceX credentials are only automatically provided on the Open Data coffea-casa instance

--- analyses/atlas-open-data-hzz/HZZ_analysis_pipeline.py
+++ analyses/atlas-open-data-hzz/HZZ_analysis_pipeline.py
@@ -88,11 +88,11 @@ IGNORE_CACHE = False
 # %% [markdown]
 # ### Required setup for this notebook
 #
-# If you are running on coffea-casa, you should be good to go: ServiceX credentials are automatically available to you.
-# If not, you will need to set those up.
+# If you are running on the coffea-casa Open Data instance, ServiceX credentials are automatically available to you.
+# Otherwise you will need to set those up.
 # Create a file `servicex.yaml` in your home directory, or the place this notebook is located in.
 #
-# See this [talk by KyungEon](https://indico.cern.ch/event/1076231/contributions/4560404/) for more information.
+# See this [talk by KyungEon](https://indico.cern.ch/event/1076231/contributions/4560404/) and the [ServiceX doc](https://servicex.readthedocs.io/en/latest/user/getting-started/) for more information.
 
 # %% [markdown]
 # ## Files to process
