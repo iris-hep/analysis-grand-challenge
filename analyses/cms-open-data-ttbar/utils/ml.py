e19f165336d1375fff613bce073ab849303a61e7
Elliott Kauffman <65742271+ekauffma@users.noreply.github.com>
2023-09-13 10:28:56 -0400

    feat: add training for cms open data ttbar  (#205)
    * adding notebook for ML model training for CMS ttbar analysis

--- analyses/cms-open-data-ttbar/utils/ml.py
+++ analyses/cms-open-data-ttbar/utils/ml.py
@@ -236,3 +236,192 @@ def get_inference_results_triton(features, even, triton_client, MODEL_NAME,
         ).as_numpy(output_name)[:, 1]
 
     return results
+
+def training_filter(jets, electrons, muons, genparts, even):
+    '''
+    Filters events down to training set and calculates jet-level labels
+    
+    Args:
+        jets: selected jets after region filter (and selecting leading four for each event)
+        electrons: selected electrons after region filter
+        muons: selected muons after region filter
+        genparts: selected genpart after region filter
+        even: whether the event is even-numbered (used to separate training events)
+    
+    Returns:
+        jets: selected jets after training filter
+        electrons: selected electrons after training filter
+        muons: selected muons after training filter
+        labels: labels of jets within an event (24=W, 6=top_hadron, -6=top_lepton)
+        even: whether the event is even-numbered
+    '''
+    #### filter genPart to valid matching candidates ####
+
+    # get rid of particles without parents
+    genpart_parent = genparts.distinctParent
+    genpart_filter = np.invert(ak.is_none(genpart_parent, axis=1))
+    genparts = genparts[genpart_filter]
+    genpart_parent = genparts.distinctParent
+
+    # ensure that parents are top quark or W
+    genpart_filter2 = ((np.abs(genpart_parent.pdgId)==6) | (np.abs(genpart_parent.pdgId)==24))
+    genparts = genparts[genpart_filter2]
+
+    # ensure particle itself is a quark
+    genpart_filter3 = ((np.abs(genparts.pdgId)<7) & (np.abs(genparts.pdgId)>0))
+    genparts = genparts[genpart_filter3]
+
+    # get rid of duplicates
+    genpart_filter4 = genparts.hasFlags("isLastCopy")
+    genparts = genparts[genpart_filter4]
+            
+        
+    #### get jet-level labels and filter events to training set
+        
+    # match jets to nearest valid genPart candidate
+    nearest_genpart = jets.nearest(genparts, threshold=0.4)
+    nearest_parent = nearest_genpart.distinctParent # parent of matched particle
+    parent_pdgid = nearest_parent.pdgId # pdgId of parent particle
+    grandchild_pdgid = nearest_parent.distinctChildren.distinctChildren.pdgId # pdgId of particle's parent's grandchildren
+
+    grandchildren_flat = np.abs(ak.flatten(grandchild_pdgid,axis=-1)) # flatten innermost axis for convenience
+
+    # if particle has a cousin that is a lepton
+    has_lepton_cousin = (ak.sum(((grandchildren_flat%2==0) & (grandchildren_flat>10) & (grandchildren_flat<19)),
+                                axis=-1)>0)
+    # if particle has a cousin that is a neutrino
+    has_neutrino_cousin = (ak.sum(((grandchildren_flat%2==1) & (grandchildren_flat>10) & (grandchildren_flat<19)),
+                                  axis=-1)>0)
+
+    # if a particle has a lepton cousin and a neutrino cousin
+    has_both_cousins = ak.fill_none((has_lepton_cousin & has_neutrino_cousin), False).to_numpy()
+
+    # get labels from parent pdgId (fill none with 100 to filter out events with those jets)
+    labels = np.abs(ak.fill_none(parent_pdgid,100).to_numpy())
+    labels[has_both_cousins] = -6 # assign jets with both cousins as top_lepton (not necessarily antiparticle)
+
+    training_event_filter = (np.sum(labels,axis=1)==48) # events with a label sum of 48 have the correct particles
+            
+    # filter events
+    jets = jets[training_event_filter]
+    electrons = electrons[training_event_filter]
+    muons = muons[training_event_filter]
+    labels = labels[training_event_filter]
+    even = even[training_event_filter]
+    
+    return jets, electrons, muons, labels, even
+
+
+def get_training_set(jets, electrons, muons, labels):
+    '''
+    Get features for each of the 12 combinations per event and calculates their corresponding combination-level labels
+    
+    Args:
+        jets: selected jets after training filter
+        electrons: selected electrons after training filter
+        muons: selected muons after training filter
+        labels: jet-level labels output by training_filter
+    
+    Returns:
+        features, labels (flattened to remove event level)
+    '''
+    
+    permutations_dict, labels_dict = get_permutations_dict(4, include_labels=True)
+    
+    features, perm_counts = get_features(jets, electrons, muons, max_n_jets=4)
+    
+    #### calculate combination-level labels ####
+    permutation_labels = np.array(labels_dict[4])
+    
+    # which combination does the truth label correspond to?
+    which_combination = np.zeros(len(jets), dtype=int)
+    # no correct matches
+    which_anti_combination = np.zeros(labels.shape[0], dtype=int)
+    for i in range(12):
+        which_combination[(labels==permutation_labels[i,:]).all(1)] = i
+        which_anti_combination[np.invert((labels==permutation_labels[i,:]).any(1))] = i
+
+    # convert to combination-level truth label (-1, 0 or 1)
+    which_combination = list(zip(range(len(jets),), which_combination))
+    which_anti_combination = list(zip(range(labels.shape[0],), which_anti_combination))
+    
+    truth_labels = -1*np.ones((len(jets),12))
+    for i,tpl in enumerate(which_combination):
+        truth_labels[tpl]=1
+    for i,tpl in enumerate(which_anti_combination):
+        truth_labels[tpl]=0
+        
+    #### flatten to combinations (easy to unflatten since each event always has 12 combinations) ####
+    labels = truth_labels.reshape((truth_labels.shape[0]*truth_labels.shape[1],1))
+    
+    return features, labels, which_combination
+
+# for generating triton config text
+def write_triton_config(
+    model_name, 
+    n_features, 
+    backend_name = "fil", 
+    model_type = "xgboost", 
+    max_batch_size = 50000000, 
+    predict_proba = "true"
+):
+    n_out = 1
+    if predict_proba=="true":
+        n_out = 2
+        
+    return (f"name: \"{model_name}\"\n" 
+            + f"backend: \"{backend_name}\"\n"
+            + f"max_batch_size: {max_batch_size}\n"
+            + "input [\n"
+            + " {\n"
+            + "    name: \"input__0\"\n"
+            + "    data_type: TYPE_FP32\n"
+            + f"    dims: [ {n_features} ]\n"
+            + " }\n"
+            + "]\n"
+            + "output [\n"
+            + " {\n"
+            + "    name: \"output__0\"\n"
+            + "    data_type: TYPE_FP32\n"
+            + f"    dims: [ {n_out} ]\n"
+            + " }\n"
+            + "]\n"
+            + "instance_group [{ kind: KIND_GPU }]\n"
+            + "parameters [\n"
+            + "  {\n"
+            + "    key: \"model_type\"\n"
+            + "    value: { string_value: "
+            + f"\"{model_type}\""
+            + " }\n"
+            + "  },\n"
+            + "  {\n"
+            + "    key: \"predict_proba\"\n"
+            + "    value: { string_value: "
+            + f"\"{predict_proba}\""
+            + " }\n"
+            + "  },\n"
+            + "  {\n"
+            + "    key: \"output_class\"\n"
+            + "    value: { string_value: \"true\" }\n"
+            + "  },\n"
+            + "  {\n"
+            + "    key: \"threshold\"\n"
+            + "    value: { string_value: \"0.5\" }\n"
+            + "  },\n"
+            + "  {\n"
+            + "    key: \"algo\"\n"
+            + "    value: { string_value: \"ALGO_AUTO\" }\n"
+            + "  },\n"
+            + "  {\n"
+            + "    key: \"storage_type\"\n"
+            + "    value: { string_value: \"AUTO\" }\n"
+            + "  },\n"
+            + "  {\n"
+            + "    key: \"blocks_per_sm\"\n"
+            + "    value: { string_value: \"0\" }\n"
+            + "  }\n"
+            + "]\n"
+            + "version_policy: { all { }}\n"
+            + "dynamic_batching { }")
+
+    
--- analyses/cms-open-data-ttbar/utils/plotting.py
+++ analyses/cms-open-data-ttbar/utils/plotting.py
@@ -405,3 +405,39 @@ def plot_data_mc(model_prediction_prefit, model_prediction_postfit, data, config
         figs.append({"figure": fig, "region": channel_name})
 
     return figs
+
+def plot_training_variables(all_correct, some_correct, none_correct):
+    
+    import hist
+    from .config import config as config
+    config = config["ml"]
+    
+    legend_list = ["All Matches Correct", "Some Matches Correct", "No Matches Correct"]
+    
+    n_features = all_correct.shape[1]
+        
+    # plot features on grid
+    fig, axs = plt.subplots(10,2,figsize=(14,40))
+    for i in range(n_features):
+        if i<10: 
+            column=0
+            row=i
+        else: 
+            column=1
+            row=i-10
+            
+        # define histogram
+        h = hist.Hist(
+            hist.axis.Regular(50, config["BIN_LOW"][i], config["BIN_HIGH"][i], name='observable', label=config["FEATURE_DESCRIPTIONS"][i], flow=False),
+            hist.axis.StrCategory(legend_list, name="truthlabel", label="Truth Label"),
+        )
+        
+        # fill histogram
+        h.fill(observable = all_correct[:,i], truthlabel="All Matches Correct")
+        h.fill(observable = some_correct[:,i], truthlabel="Some Matches Correct")
+        h.fill(observable = none_correct[:,i], truthlabel="No Matches Correct")
+        
+        h.plot(density=True, ax=axs[row,column])
+        axs[row, column].legend(legend_list, frameon=False)
+    
+    fig.show()
