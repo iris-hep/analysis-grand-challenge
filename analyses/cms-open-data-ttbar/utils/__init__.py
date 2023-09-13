2e6d3c1ae269814a4c54f3290c6cf1fa09cdb9ee
Alexander Held <alexander.held@cern.ch>
2023-09-13 09:24:11 -0500

    linting fix

--- analyses/cms-open-data-ttbar/utils/__init__.py
+++ analyses/cms-open-data-ttbar/utils/__init__.py
@@ -1,6 +1,6 @@
 from . import clients as clients
 from .config import config as config
-from .config_training import config as config_training
+from .config_training import config as config_training  # noqa: F401
 from . import file_input as file_input
 from . import file_output as file_output
 from . import metrics as metrics
